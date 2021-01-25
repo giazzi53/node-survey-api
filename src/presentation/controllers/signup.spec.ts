2f64d50d23031132fe4357f59ca37ad7a5959dd2
Guilherme Giazzi <p000ggiazzi@prservicos.com.br>
2021-01-25 10:39:06 -0300

    chore: fix config eslint

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,32 +1,32 @@
-import { SignUpController } from "./signup";
+import { SignUpController } from './signup'
 
-describe("SignUp Controller", () => {
-  test("Should return 400 if no name is provided", () => {
+describe('SignUp Controller', () => {
+  test('Should return 400 if no name is provided', () => {
     // sut = system under test
-    const sut = new SignUpController();
+    const sut = new SignUpController()
     const httpRequest = {
       body: {
-        email: "email@email.com",
-        passowrd: "pass123",
-        passowrdConfirmation: "pass123",
-      },
-    };
-    const httpResponse = sut.handle(httpRequest);
-    expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error("Missing param: name"));
-  });
+        email: 'email@email.com',
+        passowrd: 'pass123',
+        passowrdConfirmation: 'pass123'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+  })
 
-  test("Should return 400 if no email is provided", () => {
-    const sut = new SignUpController();
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
     const httpRequest = {
       body: {
-        name: "myname",
-        passowrd: "pass123",
-        passowrdConfirmation: "pass123",
-      },
-    };
-    const httpResponse = sut.handle(httpRequest);
-    expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error("Missing param: email"));
-  });
-});
+        name: 'myname',
+        passowrd: 'pass123',
+        passowrdConfirmation: 'pass123'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+  })
+})
