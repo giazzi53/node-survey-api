c06f9c092feeec413f7ac84dcc407aee624dc25a
Guilherme Giazzi <p000ggiazzi@prservicos.com.br>
2021-01-25 13:10:57 -0300

    chore: init tsoa routes config

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,9 +2,11 @@ import { MissingParamError } from '../errors/misssing-param-error'
 import { badRequest } from '../helpers/http-helper'
 import { SignUpRequest } from '../interfaces/signUp/signUpRequest'
 import { HttpResponse } from '../protocols/http'
-import { Body } from 'tsoa'
+import { Body, Post, Route } from 'tsoa'
 
+@Route('auth')
 export class SignUpController {
+  @Post('/sign-up')
   public signUp (@Body() signUpRequest: SignUpRequest): HttpResponse {
     for (const field in signUpRequest) {
       if (!signUpRequest[field]) {
