6c9cd370f6456f80d79833dfdf209152176b46ee
Guilherme Giazzi <p000ggiazzi@prservicos.com.br>
2021-01-25 13:24:10 -0300

    refactor: remove tsoa

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,10 +2,9 @@ import { MissingParamError } from '../errors/misssing-param-error'
 import { badRequest } from '../helpers/http-helper'
 import { SignUpRequest } from '../interfaces/signUp/signUpRequest'
 import { HttpResponse } from '../protocols/http'
-import { Body } from 'tsoa'
 
 export class SignUpController {
-  public signUp (@Body() signUpRequest: SignUpRequest): HttpResponse {
+  public signUp (signUpRequest: SignUpRequest): HttpResponse {
     for (const field in signUpRequest) {
       if (!signUpRequest[field]) {
         return badRequest(new MissingParamError(field))
